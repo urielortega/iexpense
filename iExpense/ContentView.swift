46f43a1abb36a217b05090ae2e3b0b9a447f692a
Uriel Ortega <urielortega2509@gmail.com>
2023-05-03 20:28:15 -0600

    Initial Commit

---
+++ iExpense/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Uriel Ortega on 03/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Uriel Ortega on 03/05/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
